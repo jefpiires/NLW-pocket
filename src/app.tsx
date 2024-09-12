ce3c0eb23ef9f2fd4c3f5b5b88e38ac6df151d3e
jefpiires <jeferson.piires@gmail.com>
2024-09-11 23:01:25 -0300

    Aula 3 Final

---
+++ src/app.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import { CreateGoal } from './components/create-goal'
+import { EmpityGoals } from './components/empity-goals'
+import { Summary } from './components/summary'
+import { Dialog } from './components/ui/dialog'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
+export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmpityGoals />}
+
+      <CreateGoal />
+    </Dialog>
+  )
+}
